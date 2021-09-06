ce141e2364f4ced479cc66ba4c9a9584aa6b73e1
ASVIEST <71895914+ASVIEST@users.noreply.github.com>
2021-09-06 19:26:59 +0300

    added reverse

--- paud/audio.py
+++ paud/audio.py
@@ -26,6 +26,7 @@ def get_os():
 
 class Audio:
     def __init__(self, *args, **kwargs):
+
         self.frames = (
             list(args[0])
             if len(args) == 1 and isinstance(args[0], (list, tuple))
@@ -46,7 +47,7 @@ class Audio:
         else:
             self.frame_rate = kwargs.setdefault("frame_rate", 44.1 * 1000)
             self.channels = kwargs.setdefault("channels", 1)
-            if args or kwargs:
+            if args or kwargs.get("sample_width"):
                 self.sample_width = kwargs.setdefault(
                     "sample_width", len(bytes(self.max()))
                 )
@@ -54,7 +55,7 @@ class Audio:
                 self.sample_width = 2
 
     @classmethod
-    def open(cls, file, old_reader=False):
+    def open(cls, file):
 
         if isinstance(file, pathlib.Path):
             file = io.FileIO(file, "rb")
@@ -66,16 +67,13 @@ class Audio:
                 frame_count = f.getnframes()
                 channels = f.getnchannels()
 
-                if old_reader:
-                    frames = [Frame(f.readframes(1)) for i in range(frame_count)]
-                else:
-                    frame_size = sample_width * channels
+                frame_size = sample_width * channels
 
-                    content = f.readframes(frame_count)
-                    frames = [
-                        Frame(content[0 + i : frame_size + i])
-                        for i in range(0, frame_count * frame_size, frame_size)
-                    ]
+                content = f.readframes(frame_count)
+                frames = [
+                    Frame(content[0 + i : frame_size + i])
+                    for i in range(0, frame_count * frame_size, frame_size)
+                ]
 
         return cls(
             sample_width=sample_width,
@@ -96,14 +94,6 @@ class Audio:
     def peak_amplitude(self):
         return 256 ** self.sample_width / 2
 
-    # @property
-    # def sample_width(self):
-    #     default = 16
-    #     if self.frames:
-    #         return len(bytes(self.max()))
-    #     else:
-    #         return default
-
     @property
     def frame_count(self):
         return len(self.frames)
@@ -120,8 +110,6 @@ class Audio:
         )
 
     def play(self):
-        # data = self.frames_to_data(self.frames)
-
         Play(
             get_os(),
             ToData(self.frames, self.params, lib).data,
@@ -188,3 +176,9 @@ class Audio:
 
     def __gt__(self, other):
         return self.frames > other.frames
+
+    def __eq__(self, other):
+        return self.frames == other.frames
+
+    def reverse(self):
+        return Audio(frames=reversed(self.frames), params=self.params)
